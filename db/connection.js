bbb67dce802b6afc4045edd2de8f2a40e37c7cf7
arunnikka <inda.fadila_ti23@nusaputra.ac.id>
2024-11-25 21:49:53 +0700

    rafli

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "mahasiswa_db",
+  database: "db_mahasiswa",
 });
 
 db.getConnection((err, connection) => {
